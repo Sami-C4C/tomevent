feccdb3dc3374366353f711ff05256a31932542f
Osama Ahmad <ahmado@th-brandenburg.de>
2024-01-13 08:48:53 +0100

    CustomerTooYoungException.

--- src/main/java/de/thb/dim/eventTom/valueObjects/customerManagement/exceptions/CustomerTooYoungException.java
+++ src/main/java/de/thb/dim/eventTom/valueObjects/customerManagement/exceptions/CustomerTooYoungException.java
@@ -2,15 +2,15 @@ package de.thb.dim.eventTom.valueObjects.customerManagement.exceptions;
 
 public class CustomerTooYoungException extends Exception {
 
-		/**
-	 * 
+	/**
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public CustomerTooYoungException () {
 		super("The Customer is not an adult");
 	}
-		
+
 	public CustomerTooYoungException (String message) {
 		super(message);
 	}
