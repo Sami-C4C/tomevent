e6702fa870f59341aa388936f3e1b6138a7f419d
Osama Ahmad <ahmado@th-brandenburg.de>
2024-01-13 09:33:35 +0100

    BackstageTicketVOTest updated.

--- src/test/java/de/thb/dim/eventTom/valueObjects/ticketSale/BackstageTicketVOTest.java
+++ src/test/java/de/thb/dim/eventTom/valueObjects/ticketSale/BackstageTicketVOTest.java
@@ -23,8 +23,8 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class BackstageTicketVOTest {
 
-    private BackstageTicketVO ticket1, ticket2;
-    private TicketVO ticket3;
+    private BackstageTicketVO backstageTicket1, backStageTicket2;
+    private TicketVO seatTicket;
     private CustomerVO customer1, customer2;
     private EventVO party;
     private EventVO show;
@@ -53,40 +53,40 @@ class BackstageTicketVOTest {
         show = new ShowVO(7, "Show 1", showEquipment, "Theater ABC", showDate, runtime, 1);
         party = new PartyVO(1, "Party 1", partyEquipment, "Club XYZ", partyDate, "Buffet", "DJ John");
 
-        ticket1 = new BackstageTicketVO(224, 45.60f, "B23", show, customer1);
-        ticket2 = new BackstageTicketVO(212, 45.60f, "B21", show, customer2);
-        ticket3 = new SeatTicketVO(123, 40.45f, "A20", party);
+        backstageTicket1 = new BackstageTicketVO(224, 45.60f, "B23", show, customer1);
+        backStageTicket2 = new BackstageTicketVO(212, 45.60f, "B21", show, customer2);
+        seatTicket = new SeatTicketVO(123, 40.45f, "A20", party);
     }
 
 
     @Test
     void test_equals_withSameObject_returnsTrue() {
-        assertTrue(ticket1.equals(ticket1));
+        assertTrue(backstageTicket1.equals(backstageTicket1));
     }
 
     @Test
     void test_equals_withNull_returnsFalse() {
-        assertFalse(ticket1.equals(null));
+        assertFalse(backstageTicket1.equals(null));
     }
 
     @Test
     void test_equals_for_SuperAndSubClass_objects() {
-        assertFalse(ticket1.equals(ticket2));
-        assertFalse(ticket2.equals(ticket1));
+        assertFalse(backstageTicket1.equals(backStageTicket2));
+        assertFalse(backStageTicket2.equals(backstageTicket1));
     }
 
     @Test
     void test_equals_withDifferentClass_returnsFalse() {
-        assertFalse(ticket1.equals(new Object()));
-        assertFalse(ticket2.equals(new Object()));
+        assertFalse(backstageTicket1.equals(new Object()));
+        assertFalse(backStageTicket2.equals(new Object()));
     }
 
     @Test
     void test_equals_withNullCustomer_returnFalse() throws CustomerNoDateOfBirthException, CustomerTooYoungException {
         CustomerVO anotherCustomer = new CustomerVO("Ahmad", "Osama", "Brunnenstr", 52, Gender.M, LocalDate.of(1990, 1, 2));
         BackstageTicketVO anotherTicket = new BackstageTicketVO(1, 100.0f, "E44", show, anotherCustomer);
-        ticket1.setCustomer(null);
-        assertFalse(anotherTicket.getCustomer().equals(ticket1.getCustomer()));
+        backstageTicket1.setCustomer(null);
+        assertFalse(anotherTicket.getCustomer().equals(backstageTicket1.getCustomer()));
     }
 
     @Test
@@ -94,7 +94,7 @@ class BackstageTicketVOTest {
         CustomerVO anotherCustomer = new CustomerVO("Ahmad", "Osama", "Brunnenstr", 52, Gender.M, LocalDate.of(1990, 1, 2));
 
         BackstageTicketVO anotherTicket = new BackstageTicketVO(1, 100.0f, "E33", show, anotherCustomer);
-        assertFalse(ticket1.getCustomer().equals(anotherTicket.getCustomer()));
+        assertFalse(backstageTicket1.getCustomer().equals(anotherTicket.getCustomer()));
     }
 
     @Test
@@ -103,54 +103,54 @@ class BackstageTicketVOTest {
 
         BackstageTicketVO anotherTicket = new BackstageTicketVO(1, 100.0f, "E33", show, anotherCustomer);
 
-        anotherTicket.setCustomer(ticket2.getCustomer());
-        assertEquals(anotherTicket.getCustomer(), ticket2.getCustomer());
+        anotherTicket.setCustomer(backStageTicket2.getCustomer());
+        assertEquals(anotherTicket.getCustomer(), backStageTicket2.getCustomer());
     }
 
 
     @Test
     void test_equals_withDifferentNumber_returnsFalse() {
         BackstageTicketVO differentNumberTicket = new BackstageTicketVO(2, 100.0f, "1A", show, customer1);
-        assertFalse(ticket1.equals(differentNumberTicket));
+        assertFalse(backstageTicket1.equals(differentNumberTicket));
     }
 
     @Test
     void test_equals_withDifferentPrice_returnsFalse() {
         BackstageTicketVO differentPriceTicket = new BackstageTicketVO(1, 150.0f, "1A", show, customer1);
-        assertFalse(ticket1.equals(differentPriceTicket));
+        assertFalse(backstageTicket1.equals(differentPriceTicket));
     }
 
     @Test
     void test_equals_withDifferentSeat_returnsFalse() {
         BackstageTicketVO differentSeatTicket = new BackstageTicketVO(1, 100.0f, "2B", show, customer1);
-        assertFalse(ticket1.equals(differentSeatTicket));
+        assertFalse(backstageTicket1.equals(differentSeatTicket));
     }
 
     @Test
     void test_equals_withDifferentEvent_returnsFalse() {
         EventVO differentEvent = new ShowVO(2, "Event Name", new String[]{"Sound", "Lighting"}, "Club Treptower-Park", LocalDateTime.now(), runtime, 3);
         BackstageTicketVO differentEventTicket = new BackstageTicketVO(1, 100.0f, "1A", differentEvent, customer1);
-        assertFalse(ticket1.equals(differentEventTicket));
+        assertFalse(backstageTicket1.equals(differentEventTicket));
     }
 
     @Test
     void test_equals_withDifferentEventName_returnsFalse() {
         EventVO differentEventName = new ShowVO(2, "Matrix-Berlin", new String[]{"Sound", "Lighting"}, "Club Treptower-Park", LocalDateTime.now(), runtime, 3);
         BackstageTicketVO differentEventNameTicket = new BackstageTicketVO(1, 100.0f, "1A", differentEventName, customer1);
-        assertFalse(ticket1.equals(differentEventNameTicket));
+        assertFalse(backstageTicket1.equals(differentEventNameTicket));
     }
 
     @Test
     void test_equals_withDifferentEventDate_returnsFalse() {
         EventVO differentEventDate = new ShowVO(1, "Event Name", new String[]{"Equipment"}, "Venue", LocalDateTime.now().plusDays(1), runtime, 3);
         BackstageTicketVO differentEventDateTicket = new BackstageTicketVO(1, 100.0f, "1A", differentEventDate, customer1);
-        assertFalse(ticket1.equals(differentEventDateTicket));
+        assertFalse(backstageTicket1.equals(differentEventDateTicket));
     }
 
     @Test
     void test_equals_withNullCustomer_returnsFalse() {
         BackstageTicketVO nullCustomerTicket = new BackstageTicketVO(1, 100.0f, "1A", show, null);
-        assertFalse(ticket1.equals(nullCustomerTicket));
+        assertFalse(backstageTicket1.equals(nullCustomerTicket));
     }
 
 
@@ -159,7 +159,7 @@ class BackstageTicketVOTest {
         CustomerVO differentCustomerAttributes = new CustomerVO("Schneider", "Tom", "Brunnenstr", 55, Gender.M, LocalDate.of(1990, 1, 2));
 
         BackstageTicketVO differentCustomerAttributesTicket = new BackstageTicketVO(1, 100.0f, "1A", show, differentCustomerAttributes);
-        assertFalse(ticket1.equals(differentCustomerAttributesTicket));
+        assertFalse(backstageTicket1.equals(differentCustomerAttributesTicket));
     }
 
     @Test
@@ -167,61 +167,61 @@ class BackstageTicketVOTest {
         TicketVO subclassTicket = new BackstageTicketVO(1, 100.0f, "1A", show, customer1) {
             // Anonymous subclass with no changes
         };
-        assertFalse(ticket1.equals(subclassTicket));
+        assertFalse(backstageTicket1.equals(subclassTicket));
     }
 
 
     @Test
     void test_equals_withSelfAndNullCustomer_returnsTrue() {
-        ticket1.setCustomer(null);
-        assertTrue(ticket1.equals(ticket1));
+        backstageTicket1.setCustomer(null);
+        assertTrue(backstageTicket1.equals(backstageTicket1));
     }
 
     @Test
     void test_equals_withBothNullCustomers_returnsTrue() {
-        ticket1.setCustomer(null);
+        backstageTicket1.setCustomer(null);
         BackstageTicketVO anotherTicketWithNullCustomer = new BackstageTicketVO(224, 45.60f, "E23", show, null);
-        assertTrue(ticket1.equals(anotherTicketWithNullCustomer));
+        assertTrue(backstageTicket1.equals(anotherTicketWithNullCustomer));
     }
 
     @Test
     void test_equals_withOneNullCustomer_returnsFalse() {
-        ticket1.setCustomer(null);
-        assertFalse(ticket1.equals(ticket2));
+        backstageTicket1.setCustomer(null);
+        assertFalse(backstageTicket1.equals(backStageTicket2));
     }
 
     @Test
     void test_equals_withDifferentNonNullCustomers_returnsFalse() {
         // Assuming customer2 is already initialized and different from customer1
-        ticket2.setCustomer(customer2);
-        assertFalse(ticket1.equals(ticket2));
+        backStageTicket2.setCustomer(customer2);
+        assertFalse(backstageTicket1.equals(backStageTicket2));
     }
 
     @Test
     void test_equals_withSameNonNullCustomers_returnsTrue() {
         // Assuming customer1 is already initialized and set in ticket1
         BackstageTicketVO anotherTicketWithSameCustomer = new BackstageTicketVO(224, 45.60f, "E23", show, customer1);
-        assertTrue(ticket1.equals(anotherTicketWithSameCustomer));
+        assertTrue(backstageTicket1.equals(anotherTicketWithSameCustomer));
     }
 
     @Test
     void test_equals_withAllFieldsEqual_returnsTrue() {
         // Assuming customer1 and party are already initialized and used in ticket1
         BackstageTicketVO anotherTicket = new BackstageTicketVO(224, 45.60f, "E23", show, customer1);
-        assertTrue(ticket1.equals(anotherTicket));
+        assertTrue(backstageTicket1.equals(anotherTicket));
     }
 
 
     @Test
     void test_equals_withOneNullAndOneNonNullCustomer_returnsFalse() {
         BackstageTicketVO ticketWithNullCustomer = new BackstageTicketVO(224, 45.60f, "E23", show, null);
-        assertFalse(ticketWithNullCustomer.equals(ticket1), "Tickets should not be equal if one customer is null and the other is not.");
+        assertFalse(ticketWithNullCustomer.equals(backstageTicket1), "Tickets should not be equal if one customer is null and the other is not.");
     }
 
     @Test
     void test_equals_DifferentClass_ReturnsFalse() {
 
-        assertFalse(ticket1.equals(ticket3), "Should return false when comparing with a different class object.");
+        assertFalse(backstageTicket1.equals(seatTicket), "Should return false when comparing with a different class object.");
         Object obj = new Object();
         assertFalse(obj instanceof BackstageTicketVO);
     }
@@ -230,21 +230,21 @@ class BackstageTicketVOTest {
     void test_equals_withDifferentNonNullCustomers_returnsFalse2() throws CustomerNoDateOfBirthException, CustomerTooYoungException {
         CustomerVO differentCustomer = new CustomerVO("Doe", "Jane", "Some Street", 123, Gender.F, LocalDate.of(1995, 5, 15));
         BackstageTicketVO ticketWithDifferentCustomer = new BackstageTicketVO(224, 45.60f, "E23", show, differentCustomer);
-        assertFalse(ticket1.equals(ticketWithDifferentCustomer), "Tickets should not be equal if customers are different.");
+        assertFalse(backstageTicket1.equals(ticketWithDifferentCustomer), "Tickets should not be equal if customers are different.");
     }
 
     @Test
     void test_equals_withSameNonNullCustomers_returnsTrue2() {
         CustomerVO sameCustomer = customer1; // Assuming customer1 is already initialized in the setup
         BackstageTicketVO ticketWithSameCustomer = new BackstageTicketVO(224, 45.60f, "E23", show, sameCustomer);
-        assertTrue(ticket1.equals(ticketWithSameCustomer), "Tickets should be equal if all attributes including customers are equal.");
+        assertTrue(backstageTicket1.equals(ticketWithSameCustomer), "Tickets should be equal if all attributes including customers are equal.");
     }
 
 
     @Test
     void test_equals_withObjectOfDifferentClass_returnsFalse() {
         // Assuming there's another class that extends TicketVO but is not BackstageTicketVO
-        TicketVO differentClassObject = new TicketVO(ticket3.getNumber(), ticket3.getId(), ticket3.getBasePrice(), show) {
+        TicketVO differentClassObject = new TicketVO(seatTicket.getNumber(), seatTicket.getId(), seatTicket.getBasePrice(), show) {
             // Implementing abstract methods for the sake of the example
             @Override
             public float getCharge() {
@@ -257,22 +257,22 @@ class BackstageTicketVOTest {
             }
         };
 
-        assertFalse(ticket2.equals(differentClassObject), "Should return false when compared with an object of a different class");
+        assertFalse(backStageTicket2.equals(differentClassObject), "Should return false when compared with an object of a different class");
     }
 
 
     @Test
     void test_setAndGetCustomer_returnsCorrectCustomer() throws CustomerNoDateOfBirthException, CustomerTooYoungException {
         CustomerVO newCustomer = new CustomerVO("Schneider", "Tom", "Brunnenstr", 55, Gender.M, LocalDate.of(1990, 1, 2));
-        ticket1.setCustomer(newCustomer);
-        assertEquals(newCustomer, ticket1.getCustomer());
+        backstageTicket1.setCustomer(newCustomer);
+        assertEquals(newCustomer, backstageTicket1.getCustomer());
     }
 
 
     @Test
     void test_clone_whenCloneThrows_throwsInternalError() {
         // Since we're not expecting CloneNotSupportedException to be thrown, the test case would fail if it was.
-        assertDoesNotThrow(() -> ticket1.clone());
+        assertDoesNotThrow(() -> backstageTicket1.clone());
     }
 
 
@@ -280,7 +280,7 @@ class BackstageTicketVOTest {
     void test_compareTo_withHigherId_returnsPositive() {
 
         BackstageTicketVO higherIdTicket = new BackstageTicketVO(400, 100.0f, "1A", show, customer1);
-        assertTrue(higherIdTicket.compareTo(ticket2) > 0);
+        assertTrue(higherIdTicket.compareTo(backStageTicket2) > 0);
     }
 
     @Test
@@ -301,31 +301,31 @@ class BackstageTicketVOTest {
 
     @Test
     void test_hashCode_sameAttributes_returnsSameHashCode() {
-        BackstageTicketVO anotherTicket = new BackstageTicketVO(ticket1.getNumber(), ticket1.getBasePrice(), ticket1.getSeat(), show, customer1);
-        assertEquals(ticket1.hashCode(), anotherTicket.hashCode(), "Hashcodes should match for tickets with identical attributes");
+        BackstageTicketVO anotherTicket = new BackstageTicketVO(backstageTicket1.getNumber(), backstageTicket1.getBasePrice(), backstageTicket1.getSeat(), show, customer1);
+        assertEquals(backstageTicket1.hashCode(), anotherTicket.hashCode(), "Hashcodes should match for tickets with identical attributes");
     }
 
     @Test
     void test_hashCode_differentAttributes_returnsDifferentHashCode() {
-        BackstageTicketVO differentTicket = new BackstageTicketVO(ticket2.getNumber(), ticket2.getBasePrice(), "Different Seat", show, customer2);
-        assertNotEquals(ticket1.hashCode(), differentTicket.hashCode(), "Hashcodes should not match for tickets with different attributes");
+        BackstageTicketVO differentTicket = new BackstageTicketVO(backStageTicket2.getNumber(), backStageTicket2.getBasePrice(), "Different Seat", show, customer2);
+        assertNotEquals(backstageTicket1.hashCode(), differentTicket.hashCode(), "Hashcodes should not match for tickets with different attributes");
     }
 
     @Test
     void test_hashCode_changesWithDifferentCustomer() throws CustomerNoDateOfBirthException, CustomerTooYoungException {
-        int originalHashCode = ticket1.hashCode();
+        int originalHashCode = backstageTicket1.hashCode();
         CustomerVO newCustomer = new CustomerVO("Doe", "Jane", "Some Street", 123, Gender.F, LocalDate.of(1995, 5, 15));
-        ticket1.setCustomer(newCustomer);
-        int newHashCode = ticket1.hashCode();
+        backstageTicket1.setCustomer(newCustomer);
+        int newHashCode = backstageTicket1.hashCode();
         assertNotEquals(originalHashCode, newHashCode, "Hashcode should change when customer changes.");
     }
 
     @Test
     void test_hashCode_WithNullCustomer() throws CustomerNoDateOfBirthException, CustomerTooYoungException {
-        int oldHashCode = ticket1.hashCode();
+        int oldHashCode = backstageTicket1.hashCode();
 
-        ticket1.setCustomer(null);
-        int newHashCode = ticket1.hashCode();
+        backstageTicket1.setCustomer(null);
+        int newHashCode = backstageTicket1.hashCode();
         assertNotEquals(oldHashCode, newHashCode, "Hashcode should change when customer changes.");
     }
 
@@ -333,23 +333,51 @@ class BackstageTicketVOTest {
     @Test
     void test_getCharge_alwaysReturnsExpectedValue() {
         final float expectedCharge = 1.2f; // The charge defined in my BackstageTicketVO class
-        assertEquals(expectedCharge, ticket1.getCharge(), "getCharge should return the predefined charge value");
+        assertEquals(expectedCharge, backstageTicket1.getCharge(), "getCharge should return the predefined charge value");
     }
 
 
     @Test
     void test_getSeatOfTicket_returnsFormattedSeatString() {
-        final String expectedSeatString = ticket1.getSeat() + " +B"; // The expected format defined in your BackstageTicketVO class
-        assertEquals(expectedSeatString, ticket1.getSeatOfTicket(), "getSeatOfTicket should return the seat followed by ' +B'");
+        final String expectedSeatString = backstageTicket1.getSeat() + " +B"; // The expected format defined in your BackstageTicketVO class
+        assertEquals(expectedSeatString, backstageTicket1.getSeatOfTicket(), "getSeatOfTicket should return the seat followed by ' +B'");
     }
+
+
+
+
+    /*********************************************************************************
+     * Test setEvent of BackstageTicketVO
+     *********************************************************************************/
+    @Test
+    void test_setShowEvent_withBackstageTicket_shouldSucceed() {
+        assertDoesNotThrow(() -> backStageTicket2.setEvent(show));
+    }
+
+    @Test
+    void test_setPartyEvent_withBackstageTicket_shouldThrowException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> backstageTicket1.setEvent(party));
+        assertEquals("Event shouldn't be null,also Backstage-ticket not available for Party!", exception.getMessage());
+    }
+
+    @Test
+    void test_setNullEvent_withBackstageTicket_shouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> backstageTicket1.setEvent(null));
+
+    }
+
+
+
+
+
 
 
     @AfterEach
     public void teardown() {
         // Reset all the objects to null to ensure no state is carried over between tests
-        ticket1 = null;
-        ticket2 = null;
-        ticket3 = null;
+        backstageTicket1 = null;
+        backStageTicket2 = null;
+        seatTicket = null;
         customer1 = null;
         customer2 = null;
         party = null;
