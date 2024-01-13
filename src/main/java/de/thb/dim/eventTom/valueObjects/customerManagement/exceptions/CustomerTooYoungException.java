6a97eececfcead23fbd32c527c58e558df5c51e2
Osama Ahmad <ahmado@th-brandenburg.de>
2024-01-13 14:55:49 +0100

    CustomerTooYoungExceptionTest.

--- src/main/java/de/thb/dim/eventTom/valueObjects/customerManagement/exceptions/CustomerTooYoungException.java
+++ src/main/java/de/thb/dim/eventTom/valueObjects/customerManagement/exceptions/CustomerTooYoungException.java
@@ -5,9 +5,11 @@ public class CustomerTooYoungException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+
 	public CustomerTooYoungException () {
 		super("The Customer is not an adult");
 	}
+
 
 	public CustomerTooYoungException (String message) {
 		super(message);
