ed755ee0f04facb2464e899b86313b7d494d1dcc
Osama Ahmad <ahmado@th-brandenburg.de>
2024-01-13 08:51:33 +0100

    Gender Implementation.

--- src/main/java/de/thb/dim/eventTom/valueObjects/customerManagement/Gender.java
+++ src/main/java/de/thb/dim/eventTom/valueObjects/customerManagement/Gender.java
@@ -2,27 +2,27 @@ package de.thb.dim.eventTom.valueObjects.customerManagement;
 
 public enum Gender {
 	M(1), F(2), D(3), U(4);
-	
+
 	private final int number;
-	
+
 	private Gender(int nr) {
 		this.number = nr;
 	}
-	
+
 	public int toNumber() {
 		return number;
 	}
-	
+
 	public String toString() {
 		switch(number) {
-		case 1:
-			return "male";
-		case 2: 
-			return "female";
-		case 3:
-			return "diverse";
-		default: 
-			return "unknown";
+			case 1:
+				return "male";
+			case 2:
+				return "female";
+			case 3:
+				return "diverse";
+			default:
+				return "unknown";
 		}
 	}
 }
