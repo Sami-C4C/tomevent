2c63f577b37966690c9ba44258f8eb37a00892a6
Osama Ahmad <ahmado@th-brandenburg.de>
2024-01-13 14:59:00 +0100

    NoOrderException implementation.

--- src/main/java/de/thb/dim/eventTom/businessObjects/exceptions/NoOrderException.java
+++ src/main/java/de/thb/dim/eventTom/businessObjects/exceptions/NoOrderException.java
@@ -3,11 +3,13 @@ package de.thb.dim.eventTom.businessObjects.exceptions;
 public class NoOrderException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-	
+
+
 	public NoOrderException () { 
 		super("Order could not be found.");
 	}
-	
+
+
 	public NoOrderException(String message) {
 		super(message);
 	}
