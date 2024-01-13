7a1116a7215faee77d8ff8663c029ca086ce1ea7
Osama Ahmad <ahmado@th-brandenburg.de>
2024-01-13 10:00:05 +0100

    Redundant uploaded Package by Tobi, this is already existed into Bitbucket-repo.

--- src/de/thb/dim/eventTom/businessObjects/IService.java
+++
@@ -1,9 +0,0 @@
-package de.thb.dim.eventTom.businessObjects;
-
-import de.thb.dim.eventTom.businessObjects.exceptions.NoCustomerException;
-import de.thb.dim.eventTom.valueObjects.ticketSale.OrderVO;
-
-interface IService {
-
-	public String startService(OrderVO order) throws NoCustomerException, IllegalStateException, NullPointerException;
-}
--- src/de/thb/dim/eventTom/valueObjects/ticketSale/StateOfOrderVO.java
+++
@@ -1,5 +0,0 @@
-package de.thb.dim.eventTom.valueObjects.ticketSale;
-
-public enum StateOfOrderVO {
-	STARTED, CONFIRMED, PRINTED, MAILED, FINISHED;
-}
